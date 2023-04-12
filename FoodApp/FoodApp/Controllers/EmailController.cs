2bfc20060324c8641467d14d1b4f6b9a4bd06dca
Ivan Von Staden <ivanv@bbd.co.za>
2023-04-12 18:50:56 +0200

    Added Auth class and extended email system to send an Image with an embedded link

--- FoodApp/FoodApp/Controllers/EmailController.cs
+++ FoodApp/FoodApp/Controllers/EmailController.cs
@@ -6,38 +6,59 @@ using System.Net;
 using System.Security;
 using System.Web;
 using System.Web.Mvc;
+using FoodApp.Controllers.Utility;
+using System.IO;
+using System.Reflection;
+using System.Net.NetworkInformation;
+using System.Net.Mime;
 
 namespace FoodApp.Controllers
 {
+    [Route("/Email")]
     public class EmailController : Controller
     {
-        // GET: Email
-        public ActionResult Index()
-        {
-            return View();
-        }
 
-        public ActionResult sendEmails(string email, string parameters, string formLink)
+        private const string noreplyEmail = "johnnyblancnoreply@gmail.com";
+        private const string subject = "Lunch Mailer";
+        private const string body = "<h2>BLA BLA</h2>";
+        // GET: Email
+
+        [Route("/sendEmails")]
+        [HttpGet]
+        public ActionResult sendEmails(string email = "ivanblizz23@gmail.com", string parameters = "?name=4", string formLink= "https://google.com/")
         {
             using (MailMessage mail = new MailMessage())
             {
-                mail.From = new MailAddress("johnnyblancnoreply@gmail.com");
+                mail.From = new MailAddress(noreplyEmail);
                 mail.To.Add(email);
-                mail.Subject = "Hello World";
-                mail.Body = "<h1>Hello</h1>";
+                mail.Subject = subject;
                 mail.IsBodyHtml = true;
-
+                //string workingDirectory = Environment.CurrentDirectory;
+                //string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Resources\email.jpeg");
+                string attachmentName = @"Resources\email.jpeg"; // note lowercase extension
+                string attachmentPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, attachmentName);
+                Attachment picture = new Attachment(attachmentPath, MediaTypeNames.Image.Jpeg);
+                string contentID = "test001@host";
+                picture.ContentId = contentID;
+                mail.Attachments.Add(picture);
+                mail.Body = $"<html>" +
+                            $"<body>" +
+                            $"<a href=\"{formLink}\">" +
+                            $"<img src=\"cid:{contentID}\">" +
+                            "</a>" +
+                            "</body>" +
+                            "</html>";
                 using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
                 {
-                    SecureString theSecureString = new NetworkCredential("", "phvjwlngmzyrpqml").SecurePassword;
-                    smtp.Credentials = new NetworkCredential("johnnyblancnoreply@gmail.com", theSecureString);
+                    SecureString theSecureString = Authinator.MyAuth;
+                    smtp.Credentials = new NetworkCredential(noreplyEmail,theSecureString);
                     smtp.EnableSsl = true;
                     try
                     {
                         smtp.Send(mail);
                         ViewData["Title"] = "Email sent!";
                     }
-                    catch (System.Net.Mail.SmtpException error)
+                    catch (Exception error)
                     {
                         ViewData["Title"] = "Email Failed to send!";
                         ViewData["error"] = error.ToString();
