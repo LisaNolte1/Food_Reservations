b1c619b9a356c4b1fda4f5bed0e45481afd97641
reece-stephenson <reeces@bbd.co.za>
2023-04-14 10:42:22 +0200

    Added Azure AD authentication

--- FoodApp/FoodApp/Controllers/HomeController.cs
+++ FoodApp/FoodApp/Controllers/HomeController.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OpenIdConnect;
 
 namespace FoodApp.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public void SignOut()
+        {
+            HttpContext.GetOwinContext().Authentication.SignOut(
+                    OpenIdConnectAuthenticationDefaults.AuthenticationType,
+                    CookieAuthenticationDefaults.AuthenticationType);
         }
 
         public ActionResult About()
