8cf314fc66a2b4261117fbd22836a3bff70143a7
Lisa Nolte <lisan@bbd.co.za>
2023-04-15 15:28:45 +0200

    Added partial classes with rough methods

---
+++ FoodApp/FoodApp/Controllers/Utility/EmailUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodApp.Controllers.Utility
+{
+    public partial class Utility
+    {
+        private void GetMailingList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FoodApp/FoodApp/Controllers/Utility/LoginUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodApp.Controllers.Utility
+{
+    public partial class Utility
+    {
+        private void GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
