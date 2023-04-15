834f2ea0fbbab9cdab1add43b5845552f1df2010
Karl Haupt <karl@bbd.co.za>
2023-04-15 15:40:42 +0200

    Database Connection and SQL statements

---
+++ FoodApp/FoodApp/DbContext.cs
@@ -0,0 +1,60 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace FoodApp
+{
+    public class DbContext : IDisposable
+    {
+        private readonly string connectionString = "data source=KARLH\\SQLEXPRESS; database=foodDb; User ID =root;Password=;Integrated Security=SSPI";
+        private readonly SqlConnection connection;
+
+        public DbContext()
+        {
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (connection != null)
+            {
+                connection.Close();
+                connection.Dispose();
+            }
+        }
+
+        public DataTable ExecuteQuery(string query, SqlParameter[]? parameters)
+        {
+            DataTable result = new DataTable();
+
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                if (parameters != null && parameters.Length > 0)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                {
+                    adapter.Fill(result);
+                }
+            }
+
+            return result;
+        }
+
+        public int ExecuteNonQuery(string query, SqlParameter[] parameters)
+        {
+            using (SqlCommand command = new SqlCommand(query, connection))
+            {
+                if (parameters != null && parameters.Length > 0)
+                {
+                    command.Parameters.AddRange(parameters);
+                }
+
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
