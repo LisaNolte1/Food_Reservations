9b1cab02f6f02c3b86e3c3d57d35557fcbfe2986
Ivan Von Staden <ivanv@bbd.co.za>
2023-04-16 18:05:03 +0200

    Added Cuisine to preferences

--- FoodApp/Controllers/PreferencesController.cs
+++ FoodApp/Controllers/PreferencesController.cs
@@ -1,6 +1,7 @@
 ﻿using FoodApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
@@ -13,11 +14,32 @@ namespace FoodApp.Controllers
 
         public ActionResult Preferences()
         {
-           List<string> food = new List<string>(); //go to where and get the current menu
-            food.Add("Curry");
-            food.Add("Salad");
-            food.Add("Pizza");
-            ViewData["Food"] = food;
+            var db = new DbContext();
+            string readStatement = $"SELECT * FROM EVENTS WHERE active = 1";
+            Debug.WriteLine(readStatement);
+            var result = db.ExecuteQuery(readStatement, null).Rows;
+            List<KeyValuePair<int,string>> wedOptions = new List<KeyValuePair<int,string>>();
+            List<KeyValuePair<int,string>> thursOptions = new List<KeyValuePair<int,string>>();
+            foreach (DataRow row in result)
+            {
+                Debug.WriteLine(row[1]);
+                readStatement = $"SELECT * FROM CUISINES_OPTIONS WHERE cuisine_id = {row[1]}";
+                var cResults = result = db.ExecuteQuery(readStatement, null).Rows;
+                foreach(DataRow c in cResults)
+                {
+                    if ((int)row[2] == 1)
+                    {
+                        wedOptions.Add(new KeyValuePair<int, string>((int)c[0], c[3].ToString()));
+                    }
+                    else if ((int)row[2] == 2)
+                    {
+                        thursOptions.Add(new KeyValuePair<int, string>((int)c[0], c[3].ToString()));
+                    }
+                    
+                }
+            }
+            ViewData["thursdayOptions"] = thursOptions;
+            ViewData["wednesdayOptions"] = wedOptions;
             return View();
         }
 
@@ -28,17 +50,6 @@ namespace FoodApp.Controllers
             string emailAddress = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("preferred_username").Value;
             Debug.WriteLine(userfullname);
             Debug.WriteLine(emailAddress);
-
-
-            if (model != null)
-            {
-                //Console.Beep();
-                //Save data to database
-                if(model.Food != null)
-                {
-                    //Console.Beep();
-                }
-            }
             ////////////////////////////////////////////////////////
             
 
