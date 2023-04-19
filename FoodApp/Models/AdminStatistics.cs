8e24db544ddcd6c9158288239e1ca116df505cf5
Ryan Parker <ryanp@bbd.co.za>
2023-04-19 22:19:00 +0200

    Update AdminStatistics.cs, AdminController, and AdminView to add the Current Event Details table to the view

--- FoodApp/Models/AdminStatistics.cs
+++ FoodApp/Models/AdminStatistics.cs
@@ -1,7 +1,16 @@
-﻿namespace FoodApp.Models
+﻿using System;
+
+namespace FoodApp.Models
 {
-    public class AdminStatistics 
+    public class AdminStatistics
     {
+        public int[] eventIds { get; set; }
+        public DateTime[] eventDates { get; set; }
+        public string[] userEmails { get; set; }
+        public string[] cuisineNames { get; set; }
+        public string[] cuisineOptions { get; set; }
+        public string[] dietaryPreferences { get; set; }
+
         public string[] barChartLabels { get; set; }
         public int[] barChartData { get; set; }
 
