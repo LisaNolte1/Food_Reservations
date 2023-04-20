cee9b7a2709f48907c631d12a778029cae8f5b81
Stephen Potgieter <stephenp@bbd.co.za>
2023-04-20 13:14:36 +0100

    change to dropdown options to fix prepop issue

--- FoodApp/Controllers/PreferencesController.cs
+++ FoodApp/Controllers/PreferencesController.cs
@@ -17,7 +17,6 @@ namespace FoodApp.Controllers
         public ActionResult Preferences()
         {
             
-            //query to retrieve user id and the user's settings row 
             string emailAddress = System.Security.Claims.ClaimsPrincipal.Current.FindFirst("preferred_username").Value;
             var db = new DbContext();
             string userQuery = $"SELECT user_id FROM Users WHERE user_email = '{emailAddress}'";
@@ -32,19 +31,57 @@ namespace FoodApp.Controllers
             var viewModel = new FormSelection();
             if (prefResult.Rows.Count > 0)
             {
-                
                 DataRow prefRow = prefResult.Rows[0];
-                ViewData["DietryRequirements"] = prefRow.Field<int>("preference_id").ToString();
-                ViewData["Days"]= viewModel.Days = prefRow.Field<int>("day_id");
-                //viewModel.DietaryRequirements = prefRow.Field<int>("preference_id").ToString();
-                //viewModel.Days = prefRow.Field<int>("day_id");
-                
+                viewModel.DietaryRequirements = prefRow.Field<int>("preference_id");
+                viewModel.Days = prefRow.Field<int>("day_id");
+            }else
+            {
+                viewModel.DietaryRequirements = 4;
+                viewModel.Days = 4;
             }
 
+            // Get the weekly menu
             var menu = MainUtility.GetWeeklyMenu();
+
+            // Populate the options for the dropdown list
+            var daysList = new List<SelectListItem>();
+            daysList.Add(new SelectListItem { Text = "Wednesday", Value = "1" });
+            daysList.Add(new SelectListItem { Text = "Thursday", Value = "2" });
+            daysList.Add(new SelectListItem { Text = "Both", Value = "3" });
+            daysList.Add(new SelectListItem { Text = "None", Value = "4" });
+            ViewData["DaysList"] = daysList;
+
+            var preferenceList = new List<SelectListItem>();
+            daysList.Add(new SelectListItem { Text = "Vegetarian", Value = "1" });
+            daysList.Add(new SelectListItem { Text = "Halal", Value = "2" });
+            daysList.Add(new SelectListItem { Text = "Vegan", Value = "3" });
+            daysList.Add(new SelectListItem { Text = "None", Value = "4" });
+            ViewData["PreferencesList"] = preferenceList;
+
+            // Set the selected value for the dropdown list
+            if (viewModel.Days != null )
+            {
+                ViewData["SelectedDay"] = daysList.FirstOrDefault(x => x.Value == viewModel.Days.ToString());
+            }
+            else
+            {
+                ViewData["SelectedDay"] = daysList.LastOrDefault();
+            }
+            if (viewModel.DietaryRequirements != null)
+            {
+                ViewData["SelectedPreference"] = preferenceList.FirstOrDefault(x => x.Value == viewModel.DietaryRequirements.ToString());
+            }
+            else
+            {
+                ViewData["SelectedPreference"] = preferenceList.LastOrDefault();
+            }
+
+            // Pass the options for the menu to the view
             ViewData["wednesdayOptions"] = menu[0];
             ViewData["thursdayOptions"] = menu[1];
+
             return View(viewModel);
+
         }
         
 
@@ -64,11 +101,7 @@ namespace FoodApp.Controllers
             var userResult = db.ExecuteQuery(userQuery, null);
             DataRow res1 = userResult.Rows[0];
             int userId = res1.Field<int>("user_id");
-     
-            string prefQuery = $"SELECT preference_id FROM Preferences WHERE preference_type = '{model.DietaryRequirements.ToString()}'";
-            var prefResult = db.ExecuteQuery(prefQuery, null);
-            DataRow res2 = prefResult.Rows[0];
-            int preferenceId = res2.Field<int>("preference_id");
+            int preferenceId = model.DietaryRequirements;
             int dayId = model.Days;
 
             // check if we should update or insert
@@ -77,12 +110,12 @@ namespace FoodApp.Controllers
             if (checkResult.Rows[0][0] != DBNull.Value && Convert.ToInt32(checkResult.Rows[0][0]) > 0)
             {
                 string updateQuery = $"UPDATE Settings SET preference_id = {preferenceId}, day_id = {dayId} WHERE user_id = {userId}";
-                var updateResult = db.ExecuteQuery(updateQuery, null);
+                db.ExecuteQuery(updateQuery, null);
             }
             else
             {
                 string insertQuery = $"INSERT INTO Settings VALUES ({userId}, {dayId}, {preferenceId})";
-                var insertResult = db.ExecuteQuery(insertQuery, null);
+                db.ExecuteQuery(insertQuery, null);
             }     
             
             //Redirect to menu
--- FoodApp/Models/FormSelection.cs
+++ FoodApp/Models/FormSelection.cs
@@ -6,7 +6,7 @@ namespace FoodApp.Models
 {
     public class FormSelection
     {
-        public string DietaryRequirements { get; set; }
+        public int DietaryRequirements { get; set; }
         public int Days { get; set; }
         public int wedFood { get; set; }
         public int thursFood { get; set; }
